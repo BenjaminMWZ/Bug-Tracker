7cb3c56bd3b45396b2dd621bcd5b94a00fa3dd23
Benjamin Mao <benjaminmao@BenjamindeMacBook-Pro-10.local>
2025-04-05 14:45:13 -0500

    add static files

--- server/server/settings.py
+++ server/server/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True  # Allow frontend to interact with backend
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
