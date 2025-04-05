e6fe82ab99f119eeae61d9d8e73c7a696b8527d6
Benjamin Mao <benjaminmao@BenjamindeMacBook-Pro-10.local>
2025-04-05 16:46:24 -0500

    fix wsgi

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -1,16 +1,5 @@
-"""
-WSGI config for bug_tracker project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-import sys
-sys.path.insert(0, os.path.join(os.getcwd(), "server"))
-
 from django.core.wsgi import get_wsgi_application 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings") 
-application = get_wsgi_application() 
+application = get_wsgi_application()
