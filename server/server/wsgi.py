4c3783f6975f35510c7be89d053dac291f682869
Benjamin Mao <benjaminmao@BenjamindeMacBook-Pro-10.local>
2025-04-05 16:25:11 -0500

    fix wsgi

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -6,13 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-
 import os
-from django.core.wsgi import get_wsgi_application
 import sys
 
-# Add the project directory to the Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+# Ensure the project directory (i.e. /app/server) is in the Python path.
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ""))
 
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 application = get_wsgi_application()
