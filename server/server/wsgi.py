834717f52a4d79980eb7277c02774a1d5959ddda
Benjamin Mao <benjaminmao@BenjamindeMacBook-Pro-10.local>
2025-04-05 16:40:08 -0500

    fix wsgi again again

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 import sys
-# Now, remove the extra ".." so that project_root is Bug-Tracker/server.
-project_root = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-sys.path.insert(0, project_root)
-from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
-application = get_wsgi_application()
+sys.path.insert(0, os.path.join(os.getcwd(), "server"))
+
+from django.core.wsgi import get_wsgi_application 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings") 
+application = get_wsgi_application() 
