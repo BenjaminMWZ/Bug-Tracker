4972bb6de706baa11bd26298c0753b58650916d5
Benjamin Mao <benjaminmao@BenjamindeMacBook-Pro-10.local>
2025-04-05 16:09:40 -0500

    fix something

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+import sys
 
+# Add the project directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
 application = get_wsgi_application()
